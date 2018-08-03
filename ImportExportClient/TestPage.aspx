b222372a7b80bcaf2a91b9136b1837a6d1a6caa3
dbustosrc <dbustosrc@gmail.com>
2018-08-03 14:49:15 -0500

    Import/Export File Data Test Client

---
+++ ImportExportClient/TestPage.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestPage.aspx.cs" Inherits="ImportExportClient.TestPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Formato"></asp:Label>
+            <asp:TextBox ID="txtFormat" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Acción"></asp:Label>
+            <asp:RadioButtonList ID="rdioListOption" runat="server">
+                <asp:ListItem Selected="True">Importar</asp:ListItem>
+                <asp:ListItem>Exportar</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:Label ID="Label4" runat="server" Text="Líneas"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtFileContent" runat="server" TextMode="MultiLine"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Respuesta"></asp:Label>
+            <asp:Label ID="lblResponse" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ejecutar" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ImportExportClient/TestPage.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ImportExportClient.ImportExportFrameworkService;
+
+namespace ImportExportClient
+{
+    public partial class TestPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CallContext callContext = new CallContext();
+            NOVImportExportFormatContract dataContract = new NOVImportExportFormatContract();
+            NOVFileValueContract[] result;
+            String[] fileLines;
+            NOVImportExportFormatServiceClient serviceClient = new NOVImportExportFormatServiceClient();
+
+            if (rdioListOption.SelectedIndex == 0)
+            {
+                dataContract.ActionType = NOVImportExport.Import;
+                fileLines = txtFileContent.Text.Split('\n');
+                dataContract.FileLines = fileLines;
+            }
+            else
+            {
+                dataContract.ActionType = NOVImportExport.Export;
+            }
+
+            dataContract.FileFormatId = txtFormat.Text;
+            result = serviceClient.startOperation(callContext, dataContract);
+
+            foreach (var item in result)
+            {
+                lblResponse.Text += item.parmValue;
+            }
+
+            
+        }
+    }
+}
